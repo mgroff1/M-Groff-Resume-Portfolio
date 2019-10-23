fce27a4cf08cfb0fdd04615c763d69d028ba8536
mgroff1 <mick.groff@gmail.com>
2019-10-22 23:36:22 -0600

    Add files via upload

---
+++ template-folder/main.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var w = canvas.width;
+var h = canvas.height;
